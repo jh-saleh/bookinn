502ccd09d2e43d5e9ea103721238971093a10f61
jh-saleh <salehjh746@gmail.com>
2024-08-19 09:14:36 +0200

    ajout de la page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './page/home/home-page.component';
-import { HostPageComponent } from './page/host/host-page.component';
-import { RoomPageComponent } from './page/rooms/room-page.component';
-import { RootPageComponent } from './page/root/root-page.component';
+import { HomePageComponent } from './page/home-page/home-page.component';
+import { HostPageComponent } from './page/host-page/host-page.component';
+import { NotFoundPageComponent } from './page/not-found-page/not-found-page.component';
+import { RoomPageComponent } from './page/room-page/room-page.component';
+import { RootPageComponent } from './page/root-page/root-page.component';
 import { SearchPageComponent } from './page/search-page/search-page.component';
 
 export const routes: Routes = [
@@ -11,4 +12,5 @@ export const routes: Routes = [
     { path: 'room/:id', component: RoomPageComponent, title: 'Room | BookInn' },
     { path: 'host/:id', component: HostPageComponent, title: 'Host profile | BookInn' },
     { path: 's/:location/:type', component: SearchPageComponent, title: 'Search Page | BookInn' },
+    { path: '**', component: NotFoundPageComponent, title: 'Not Found | BookInn' },
 ];
